256e0d2e15d835190eaaa9edd5056c0e2517f04d
Augusto H. <augusto.hernandez@tfja.edu.mx>
2026-09-04 10:13:35 -0600

    Contratos Frontend.
    Worklflow Config.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  filesBaseUrl: '__FILES_BASE_URL__',
+  vectorAdminBaseUrl: '__VECTOR_ADMIN_BASE_URL__',
+  vectorSearchBaseUrl: '__VECTOR_SEARCH_BASE_URL__',
+  conversationBaseUrl: '__CONVERSATION_BASE_URL__',
+  instruccionesSistemaBaseUrl: '__INSTRUCCIONES_SISTEMA_BASE_URL__',
+  bitacoraBaseUrl: '__BITACORA_BASE_URL__',
+  authUsuario: '__AUTH_USUARIO__',
+  authContrasena: '__AUTH_CONTRASENA__',
+};
